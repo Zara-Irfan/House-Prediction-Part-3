d9f09c39c5a7f90f5c361555e3cf618e4a7a3928
Zara-Irfan <zarairfantechdev@gmail.com>
2025-08-15 22:39:21 -0400

    app.js

---
+++ Client/app.js
@@ -0,0 +1,62 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for (var i = 0; i < uiBathrooms.length; i++) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
+        }
+    }
+    return -1;
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for (var i = 0; i < uiBHK.length; i++) {
+        if (uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
+    }
+    return -1;
+}
+
+function onClickedEstimatePrice() {
+    var sqft = document.getElementById("uiSqft").value;
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations").value;
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    if (sqft === "" || bhk === -1 || bathrooms === -1 || location === "") {
+        estPrice.innerHTML = "<span style='color:red'>Please fill all fields!</span>";
+        return;
+    }
+
+    $.post("http://127.0.0.1:5000/predict_home_price", {
+        total_sqft: parseFloat(sqft),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location
+    }, function(data, status) {
+        estPrice.innerHTML = "<span>Estimated Price: <b>" + data.estimated_price + " Lakh</b></span>";
+    }).fail(function() {
+        estPrice.innerHTML = "<span style='color:red'>Error fetching price. Make sure Flask server is running.</span>";
+    });
+}
+
+function onPageLoad() {
+    $.get("http://127.0.0.1:5000/get_location_names")
+    .done(function(data) {
+        if (data && data.locations) {
+            var uiLocations = document.getElementById("uiLocations");
+            uiLocations.innerHTML = '<option value="" disabled selected>Choose a Location</option>';
+            for (var i = 0; i < data.locations.length; i++) {
+                var opt = new Option(data.locations[i], data.locations[i]);
+                uiLocations.appendChild(opt);
+            }
+        }
+    })
+    .fail(function() {
+        alert("Cannot fetch list of locations! Make sure Flask server is running.");
+    });
+}
+
+window.onload = onPageLoad;
